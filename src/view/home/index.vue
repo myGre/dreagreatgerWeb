4800f50ede5033e744157e3b126dabbecda01e00
myGre <1745083253@qq.com>
2022-09-19 21:22:24 +0800

    调整导航栏

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,48 +1,47 @@
 <template>
   <div class="home-content">
     <!-- 导航 -->
-    <div class="navs" :class="{ 'maxHeight': show }">
-      <nav :class="{ 'shake': show }">
-        <li @click="navClickItem(index)" v-for="(item, index) in navObj" :key="index" class="navItemCode">
-          <p>{{ item }}</p>
-        </li>
-        <div class="border_nav" :style="`transform: translate3d(${offsetleft}px, 0, 0);`"></div>
-      </nav>
-      <div @click="navShow" class="btnDiv">
-        <button class="icon-btn" ref="btnRef">
-          <div class="icon"></div>
-        </button>
+    <div class="navs" :class="{'maxWidth': show}">
+      <div class="nav">
+        <div class="favicon">
+          <img src="../../assets/favicon.ico" alt="">
+        </div>
+        <nav ref="navRef" :class="{ 'shake': show }">
+          <li @click="navClickItem(index)" v-for="(item, index) in navObj" :key="index" class="navItemCode"
+            :class="{ 'activeClass': index === navItem}">
+            <p>{{ item }}</p>
+          </li>
+          <div class="border_nav" :style="`transform: translate3d(${navItemOfsetLeft}px, 0, 0);`"></div>
+        </nav>
       </div>
     </div>
     <!-- 内容区 -->
     <my-mian ref="myMianRef" :isNavClick="isNavClick" @getIsNavClick="getIsNavClick" @getCurrtent="getCurrtent"
       :navItem="navItem"></my-mian>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, provide, ref, watch } from "vue";
+import { computed, onMounted, provide, ref, watch } from "vue";
 
 const navObj = ref(["首页", "资料卡", "个人信息", "相册", "学籍"]);
 const btnRef = ref();
+const navRef = ref(); ///导航
 const myMianRef = ref(); // mymain组件
 const navItem = ref<number>(0); // 当前导航
 const show = ref(false); // 是否显示导航栏
-const offsetleft = ref(0); // 边框divtop值
+const navItemOfsetLeft = ref(0); // 边框divtop值
 // 是否开启导航跳转
 const isNavClick = ref(true);
 
-watch(navItem, (newValue, oldValue) => {
-  // console.log(newValue, oldValue);
-  let difference: number;
-  difference = (newValue - oldValue) * 76;
-  offsetleft.value += difference;
-
+watch(navItem, (newValue) => {
+  navItemOfsetLeft.value = navRef.value.children[newValue].offsetLeft;
 })
-
 // 是否显示导航栏
 function navShow() {
-  btnRef.value.classList.toggle("toggled");
   show.value = !show.value;
 }
 // 获取当前盒子
@@ -58,6 +57,9 @@ function navClickItem(index: number) {
   isNavClick.value = true;
   navItem.value = index;
 }
+onMounted(() => {
+  how()
+})
 </script>
 
 <style lang="scss" scoped>
